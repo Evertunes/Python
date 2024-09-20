8a93bd7d6cffee17852474f7a89de1856f6c913e
Everton Alves <alveseverton023@gmail.com>
2024-09-20 12:14:49 -0300

    type

--- Aula3Int_float.py
+++ Aula3Int_float.py
@@ -1,6 +1,13 @@
-print (11) #int
-print (-11) #int
-print (0) #int
+#print (11) #int
+#print (-11) #int
+#print (0) #int
 
-print (1.1) #float
-print (1.1, 10.11) #float
+#print (1.1) #float
+#print (1.1, 10.11) #float
+
+#type é uma classe que executa algo #type mostra o qual o 
+#formato do que eu estou digitando, no caso <class 'str'> para Everton ou seja, é uma string
+# e para 1 vai mostrar <class 'int'> inteiro
+
+print (type('Everton')) 
+print (type(1)) 
