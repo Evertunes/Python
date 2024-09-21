3b912e37a7130d382989dd10465277820eced2ad
Everton Alves <alveseverton023@gmail.com>
2024-09-21 19:33:07 -0300

    format

--- ExAritmetica.py
+++ ExAritmetica.py
@@ -1,6 +1,14 @@
 nome = "Everton Alves"
 altura = 1.66
 peso = 70
-imc = peso // (altura * altura)
+imc = peso / (altura * altura)
 
-print(nome, "\n" "Peso:", peso, "\n" "Tem:", altura, "\nSeu IMC e: ", imc)
+# print(nome, "\n" "Peso:", peso, "\n" "Tem:", altura, "\nSeu IMC e: ", imc)
+# impressao com f-string
+linha_1 = f'{nome} tem {altura:.1f} de altura' #.1f formata com 1 casa decimal
+linha_2 = f'pesa {peso} quilos e seu IMC e: '
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
---
+++ formatacao.py
@@ -0,0 +1,7 @@
+a = 'A'
+b = 'B'
+c = 1.1
+string = 'a={0} b={1} c={2:.2f}'
+formato = string.format(a, b, c) #a b e c  são argumentos que eu passo para a format
+
+print(formato)
