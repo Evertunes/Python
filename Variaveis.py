c4331b64183b984f829356aed41ee52f5ce646ab
Everton Alves <alveseverton023@gmail.com>
2024-09-21 15:04:29 -0300

    exeVariaveis

---
+++ Variaveis.py
@@ -0,0 +1,17 @@
+nome_completo = "Everton Alves"  # crio uma variavel
+soma_dois_mais_dois = 2 + 2  # crio uma variavel
+print(nome_completo, soma_dois_mais_dois)  # exibo uma variavel
+
+#=====================================================#
+
+int_um = int('1')
+print (int_um, type(int_um))
+
+#=====================================================#
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+
+print ('Nome: ',nome, 'Idade: ', idade)
+print ('E maior?', maior_de_idade)
