b1d1ff42f42f3e63b9cdcc1e71c5d90879684d4c
Everton Alves <alveseverton023@gmail.com>
2024-09-21 14:47:03 -0300

    Conversoes

---
+++ Aula5Conversao.py
@@ -0,0 +1,6 @@
+# quando passo o numero um como dessa forma int('1'), ele deixa passa a ser int
+# por causa da declaracao de int antes do parenteses
+print(int("1"), type(int("1")))
+print(int("2") + 5)  # soma a string 2 recebeida ao inteiro 5 (conversao de str para int)
+print(bool('-')) #string vazia flase, string com algum caracter True
+print(str(11) + 'b') #converte 11 em str para imprimir 11b na tela
