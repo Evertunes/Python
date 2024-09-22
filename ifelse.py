5c01a4374dbf26d04f4cc879a336970e4ff6976e
Everton Alves <alveseverton023@gmail.com>
2024-09-21 23:17:33 -0300

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+entrada = input ('Voce que "entrar" ou "sair"?''\n')
+
+if entrada == 'entrar':
+    print ('-----------------------')
+    print ('Voce entrou no sistema!')
+    print ('-----------------------')    
+elif entrada == 'sair':
+    print ('--------------------')    
+    print ('Voce saiu do sistema!')
+    print ('--------------------')
+else:
+    print ('----------------------------------------')
+    print ('Voce não digitou nem entrar e nem sair!!')
+    print ('----------------------------------------')
