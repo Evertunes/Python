c3877626c16948f8cd7b644037e17800bc4d2950
Everton Alves <alveseverton023@gmail.com>
2024-09-22 15:37:11 -0300

    comparacao

---
+++ exComparacao.py
@@ -0,0 +1,12 @@
+numero1 = input ('Digite um numero: ')
+print('----------------------------------------')
+numero2 = input ('Digite outro numero: ')
+
+if numero1 > numero2:
+    print('----------------------------------------')
+    print(f'{numero1} e maior ou igual ao {numero2}')
+    print('----------------------------------------')
+else:
+    print('-----------------------------------------')
+    print(f'{numero2} e menor que o numero {numero1}')
+    print('-----------------------------------------')
