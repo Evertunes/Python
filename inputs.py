8f7aaa431e4ba5b9f66991e5b33a9a86d5191fb4
Everton Alves <alveseverton023@gmail.com>
2024-09-21 23:05:45 -0300

    comentarios

--- inputs.py
+++ inputs.py
@@ -5,6 +5,8 @@ print('Agora por favor...', end='\n')
 numero_1 = input("Digite um número: ")
 numero_2 = input("Digite outro número: ")
 
+#aqui eu faço uma checagem para garantir que o que eu digitei são numeros inteiros
+#converto a entrada em INT, para garantir a soma, se não for numero inteiro, gera um erro ao final do codigo
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
