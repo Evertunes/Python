0a72d7defdb9a435e97bff47969345e9a1e80e58
Everton Alves <alveseverton023@gmail.com>
2024-10-08 16:57:03 -0300

    alteracao input

--- exercicioCalculadora.py
+++ exercicioCalculadora.py
@@ -1,5 +1,6 @@
 """Calculadora com while"""
 while True:
-    sair = input('Quer sair? [s]im: ')
-    sair = sair.lower().startswith('s') #retorna a mesma string eniada em sair em letra minuscula/startswitch verifica que o que a pessoa digitou começa com a letra desejada
-    print(sair)
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')#retorna a mesma string eniada em sair em letra minuscula/startswitch verifica que o que a pessoa digitou começa com a letra desejada
+    
+    if sair is True:
+        break
