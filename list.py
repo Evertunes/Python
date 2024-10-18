2a4eaa84f87e9d746ad78b5921e72c42eea39bf3
Everton Alves <alveseverton023@gmail.com>
2024-10-18 16:12:39 -0300

    lista

---
+++ list.py
@@ -0,0 +1,14 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+string = 'ABCDE'
+#print(bool([])) #falsy
+#print(lista,type(lista))
+lista = [123,True,'Everton Alves', 1.2,[]]
+lista[2] = 'José'
+print(lista[2], type(lista[2]))
